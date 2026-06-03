dade8a365cd4349fd817f10c1b654ec1d8d14780
warnakulasuriya-fds-e23 <fdswarnakulasuriya@gmail.com>
2026-06-03 06:47:50 +0530

    remove default case

--- src/org/psnbtech/Tetris.java
+++ src/org/psnbtech/Tetris.java
@@ -229,13 +229,7 @@ public class Tetris extends JFrame {
 					}
 					break;
 				
-				default:
-					if(!isGameOver && !isNewGame) {
-						isPaused = !isPaused;
-						logicTimer.setPaused(isPaused);
-					}
-					break;
-				}
+
 			}
 			
 			@Override
